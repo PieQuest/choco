022d782ba6ed0f6ec864b451204d7dd32ca24f87
PieQuest <39866347+PieQuest@users.noreply.github.com>
2018-06-20 18:02:57 -0700

    Big Updates
    added a fully built test

--- ChocoBaseInstall.ps1
+++ ChocoBaseInstall.ps1
@@ -1,4 +1,17 @@
 ﻿# =====================================================================================================
+#                                      Set Execution Policy
+# =====================================================================================================
+
+# Testing this:
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+$rstrct = New-Object -com wscript.shell;
+#$rstrct.AppActivate('Windows PowerShell')
+Start-Sleep -s 1
+$rstrct.SendKeys('a{ENTER}')
+
+# =====================================================================================================
 #                                   .NET Framework Redundancy Check
 # =====================================================================================================
 
@@ -31,34 +44,72 @@ else { write-host "Installation for Office Removal Tool cannot be found" }
 # =====================================================================================================
 
 Write-Host Installing Chocolatety...`n
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+<#
+If (Get-ExecutionPolicy -eq Restricted)
+{
+Set-ExecutionPolicy AllSigned
+}
+#>
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Start-Sleep -s 1
+choco upgrade chocolatey
+Start-Sleep -s 1
+# Need to put repo on the NAS
+choco source add -n SPIErepo -s \\mmcmurray-7050\c$\Users\michaelm\Documents\Github\choco\repo
 # =====================================================================================================
 #                                       Coco-Public-Repo App Install
 # =====================================================================================================
 
 Write-Host Installing Coco-Public-Repo Apps...`n
 
-#right now im just working on replicating the base app install list that we already have
-#but only putting apps that we always need to keep up-to-date on there
+<#
+right now im just working on replicating the base app install list that we already have
+but only putting apps that we always need to keep up-to-date on there
+#>
 
 # Public App List:
 choco install flashplayerplugin -y
 choco install googlechrome -y
+choco install firefoxesr -y
 choco install jre8 -y
-choco install snagit /licenseCode:"ACFMR-BCMFH-YZAEW-KE78P-JC5C9"
+choco install snagit /licenseCode:"ACFMR-BCMFH-YZAEW-KE78P-JC5C9" -y
 choco install vlc -y
 choco install 7zip -y
-#choco install adobereader
+#choco install adobereader -y
+#choco install notepadplusplus -y
+#choco install quicktime -y
+#choco install filezilla -y
+#choco install itunes -y
+#choco install spotify -y
 
 
 Write-Host Coco-Public-Repo Install Complete!`n
 
 # =====================================================================================================
-#                                       Coco-Github-Repo App Install
+#                                       Coco-SPIE-Repo App Install
 # =====================================================================================================
 
-Write-Host Installing Coco-Github-Repo Apps...`n
+Write-Host Installing Coco-SPIE-Repo Apps...`n
 
+choco install office365 #change to SPIEoffice365
 #choco install adobeacrobat  #make custom
-choco install office365business #make this custom?
+#choco install office365business #make this custom?
+#SPIE DropBox
+
+
+
+
+
+# =====================================================================================================
+#                                      Set Execution Policy
+# =====================================================================================================
+
+# Test:
+Set-ExecutionPolicy -ExecutionPolicy Default
+<#
+$rstrct = New-Object -com wscript.shell;
+#$rstrct.AppActivate('Windows PowerShell')
+Start-Sleep -s 1
+$rstrct.SendKeys('a{ENTER}')
+#>
